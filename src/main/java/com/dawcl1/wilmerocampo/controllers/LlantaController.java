2d6ed4c6ccb52d23146dac96a5ede258491d878f
helzze <sebastiansancaowo@gmail.com>
2024-04-11 17:50:14 -0500

    solucion de la pregunta 1

--- src/main/java/com/dawcl1/wilmerocampo/controllers/LlantaController.java
+++ src/main/java/com/dawcl1/wilmerocampo/controllers/LlantaController.java
@@ -1,7 +1,30 @@
 package com.dawcl1.wilmerocampo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LlantaController {
+
+    @GetMapping("/inicio")
+    public String mostrarFormulario() {
+        return "llanta-formulario";
+    }
+
+    @PostMapping("/calculartotal")
+    public String calcularTotal(@RequestParam int cantidad, Model model) {
+        double totalPagar;
+        if (cantidad < 5) {
+            totalPagar = 800 * cantidad;
+        } else {
+            totalPagar = 700 * cantidad;
+        }
+        model.addAttribute("totalPagar", totalPagar);
+        model.addAttribute("cantidad", cantidad);
+        return "resultado";
+    }
+
 }
