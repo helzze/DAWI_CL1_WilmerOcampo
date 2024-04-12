b8af3d7d050214f44e0fe10772a68c700e719f5c
Genezi-00 <genesis@kaztro.cloud>
2024-04-11 22:23:55 -0500

    Se agregro diseño nav al proyecto

---
+++ src/main/java/com/dawcl1/wilmerocampo/controllers/HomeController.java
@@ -0,0 +1,10 @@
+package com.dawcl1.wilmerocampo.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
