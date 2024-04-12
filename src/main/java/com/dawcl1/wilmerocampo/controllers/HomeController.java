cc900dd1f6c911b10942a8e23e9a10464535299c
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-04-11 23:08:58 -0500

    Actualizando enlace /home en HomeController

--- src/main/java/com/dawcl1/wilmerocampo/controllers/HomeController.java
+++ src/main/java/com/dawcl1/wilmerocampo/controllers/HomeController.java
@@ -4,9 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping({"/", "/home"})
     public String home() {
         return "home";
     }
