e67dd091cb0b893c645ff17d165f39c8957a16e4
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-04-11 22:39:27 -0500

    Actualizando HomeController

--- src/main/java/com/dawcl1/wilmerocampo/controllers/HomeController.java
+++ src/main/java/com/dawcl1/wilmerocampo/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package com.dawcl1.wilmerocampo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+@RequestMapping("/home")
 public class HomeController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home() {
         return "home";
     }
